3a1e5dc63319a5138fb00be06a726b3526677d37
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-02 12:38:18 +0100

    Test after some seconds of sleep
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- test.ps1
+++ test.ps1
@@ -5,8 +5,11 @@ docker kill whoamitest
 docker rm -f whoamitest
 
 $ErrorActionPreference = 'Stop';
+Write-Host Starting container
 docker run --name whoamitest -p 8000:8000 -d whoami
 $ip=$(docker inspect -f '{{ .NetworkSettings.Networks.nat.IPAddress  }}' whoamitest)
+Start-Sleep 15
+Write-Host Testing from another container
 docker run --rm microsoft/nanoserver powershell -command invoke-webrequest -usebasicparsing http://$($ip):8000
 
 $ErrorActionPreference = 'SilentlyContinue';
