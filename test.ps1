a78cfbd1b4d4d2fa22c0634f19e321953bffa995
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-02 12:15:40 +0100

    Ignore warnings while cleaning up
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- test.ps1
+++ test.ps1
@@ -1,12 +1,14 @@
-$ErrorActionPreference = 'Stop';
 Write-Host Starting test
 
+$ErrorActionPreference = 'SilentlyContinue';
 docker kill whoamitest
 docker rm -f whoamitest
 
+$ErrorActionPreference = 'Stop';
 docker run --name whoamitest -p 8000:8000 -d whoami
 $ip=$(docker inspect -f '{{ .NetworkSettings.Networks.nat.IPAddress  }}' whoamitest)
 docker run --rm microsoft/nanoserver powershell -command invoke-webrequest -usebasicparsing http://$($ip):8000
 
+$ErrorActionPreference = 'SilentlyContinue';
 docker kill whoamitest
 docker rm -f whoamitest
