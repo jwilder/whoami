680f1c10a6968f3719a1301aca4660cfe74a9ad4
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-02 12:11:01 +0100

    Remove build.sh
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker build -t httpbuild -f Dockerfile.build .
-docker rm -f httpbuild
-docker create --name httpbuild httpbuild
-rm -rf tmp
-mkdir tmp
-docker cp httpbuild:/code/http.exe tmp
-docker build -t whoami .
