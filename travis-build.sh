615259934e908a4817372086d15d607c37cbe565
Stefan Scherer <scherer_stefan@icloud.com>
2017-04-09 17:36:11 +0200

    Fix Docker update

--- travis-build.sh
+++ travis-build.sh
@@ -2,16 +2,16 @@
 set -e
 
 echo "Updating Docker engine to master"
-service docker stop
-sudo curl -L -o /usr/local/bin/docker-containerd https://master.dockerproject.org/linux/amd64/docker-containerd
-sudo curl -L -o /usr/local/bin/docker-containerd-ctr https://master.dockerproject.org/linux/amd64/docker-containerd-ctr
-sudo curl -L -o /usr/local/bin/docker-containerd-shim https://master.dockerproject.org/linux/amd64/docker-containerd-shim
-sudo curl -L -o /usr/local/bin/dockerd https://master.dockerproject.org/linux/amd64/dockerd
-sudo curl -L -o /usr/local/bin/docker-init https://master.dockerproject.org/linux/amd64/docker-init
-sudo curl -L -o /usr/local/bin/docker-proxy https://master.dockerproject.org/linux/amd64/docker-proxy
-sudo curl -L -o /usr/local/bin/docker-runc https://master.dockerproject.org/linux/amd64/docker-runc
-sudo curl -L -o /usr/local/bin/docker https://master.dockerproject.org/linux/amd64/docker
-service docker start
+sudo service docker stop
+sudo curl -L -o /usr/bin/docker-containerd https://master.dockerproject.org/linux/amd64/docker-containerd
+sudo curl -L -o /usr/bin/docker-containerd-ctr https://master.dockerproject.org/linux/amd64/docker-containerd-ctr
+sudo curl -L -o /usr/bin/docker-containerd-shim https://master.dockerproject.org/linux/amd64/docker-containerd-shim
+sudo curl -L -o /usr/bin/dockerd https://master.dockerproject.org/linux/amd64/dockerd
+sudo curl -L -o /usr/bin/docker-init https://master.dockerproject.org/linux/amd64/docker-init
+sudo curl -L -o /usr/bin/docker-proxy https://master.dockerproject.org/linux/amd64/docker-proxy
+sudo curl -L -o /usr/bin/docker-runc https://master.dockerproject.org/linux/amd64/docker-runc
+sudo curl -L -o /usr/bin/docker https://master.dockerproject.org/linux/amd64/docker
+sudo service docker start
 docker version
 
 if [ "$ARCH" != "amd64" ]; then
