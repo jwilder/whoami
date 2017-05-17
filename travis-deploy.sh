f0c2e5ebcb7efad5b8cff8feb5bd20e183966065
Stefan Scherer <scherer_stefan@icloud.com>
2017-05-17 22:45:13 +0200

    Use docker manifest command

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -25,21 +25,24 @@ if [ "$ARCH" == "amd64" ]; then
   done
   set -e
 
-  echo "Downloading manifest-tool"
-  wget https://github.com/estesp/manifest-tool/releases/download/v0.4.0/manifest-tool-linux-amd64
-  mv manifest-tool-linux-amd64 manifest-tool
-  chmod +x manifest-tool
-  ./manifest-tool
+  echo "Downloading docker client with manifest command"
+  wget https://github.com/StefanScherer/dockerfiles-windows/releases/download/2017-05-17-docker-manifest/docker-linux-amd64
+  mv docker-linux-amd64 docker
+  chmod +x docker
 
   echo "Pushing manifest $image:$TRAVIS_TAG"
-  ./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm,linux/arm64,windows/amd64 \
-    --template "$image:OS-ARCH-$TRAVIS_TAG" \
-    --target "$image:$TRAVIS_TAG"
+  ./docker manifest create "$image:$TRAVIS_TAG" \
+    "$image:linux-amd64-$TRAVIS_TAG" \
+    "$image:linux-arm-$TRAVIS_TAG" \
+    "$image:linux-arm64-$TRAVIS_TAG" \
+    "$image:windows-amd64-$TRAVIS_TAG"
+  ./docker manifest push "$image:$TRAVIS_TAG"
 
   echo "Pushing manifest $image:latest"
-  ./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm,linux/arm64,windows/amd64 \
-    --template "$image:OS-ARCH-$TRAVIS_TAG" \
-    --target "$image:latest"
+  ./docker manifest create "$image:latest" \
+    "$image:linux-amd64-$TRAVIS_TAG" \
+    "$image:linux-arm-$TRAVIS_TAG" \
+    "$image:linux-arm64-$TRAVIS_TAG" \
+    "$image:windows-amd64-$TRAVIS_TAG"
+  ./docker manifest push "$image:latest"
 fi
