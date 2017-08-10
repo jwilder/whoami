745a4765c1920f376afa72be2b078e565d52e5c3
Stefan Scherer <scherer_stefan@icloud.com>
2017-08-10 12:55:23 +0200

    Try docker/cli#138 artifact

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -25,21 +25,29 @@ if [ "$ARCH" == "amd64" ]; then
   done
   set -e
 
-  echo "Downloading manifest-tool"
-  wget https://github.com/estesp/manifest-tool/releases/download/v0.5.0/manifest-tool-linux-amd64
-  mv manifest-tool-linux-amd64 manifest-tool
-  chmod +x manifest-tool
-  ./manifest-tool
+  echo "Downloading docker client with manifest command"
+  wget https://3501-88013053-gh.circle-artifacts.com/1/work/build/docker-linux-amd64
+  mv docker-linux-amd64 docker
+  chmod +x docker
+  ./docker version
 
   echo "Pushing manifest $image:$TRAVIS_TAG"
-  ./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm,linux/arm64,windows/amd64 \
-    --template "$image:OS-ARCH-$TRAVIS_TAG" \
-    --target "$image:$TRAVIS_TAG"
+  ./docker manifest create "$image:$TRAVIS_TAG" \
+    "$image:linux-amd64-$TRAVIS_TAG" \
+    "$image:linux-arm-$TRAVIS_TAG" \
+    "$image:linux-arm64-$TRAVIS_TAG" \
+    "$image:windows-amd64-$TRAVIS_TAG"
+  ./docker manifest annotate "$image:$TRAVIS_TAG" "$image:linux-arm-$TRAVIS_TAG" --os linux --arch arm
+  ./docker manifest annotate "$image:$TRAVIS_TAG" "$image:linux-arm64-$TRAVIS_TAG" --os linux --arch arm64
+  ./docker manifest push "$image:$TRAVIS_TAG"
 
   echo "Pushing manifest $image:latest"
-  ./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm,linux/arm64,windows/amd64 \
-    --template "$image:OS-ARCH-$TRAVIS_TAG" \
-    --target "$image:latest"
+  ./docker manifest create "$image:latest" \
+    "$image:linux-amd64-$TRAVIS_TAG" \
+    "$image:linux-arm-$TRAVIS_TAG" \
+    "$image:linux-arm64-$TRAVIS_TAG" \
+    "$image:windows-amd64-$TRAVIS_TAG"
+  ./docker manifest annotate "$image:latest" "$image:linux-arm-$TRAVIS_TAG" --os linux --arch arm
+  ./docker manifest annotate "$image:latest" "$image:linux-arm64-$TRAVIS_TAG" --os linux --arch arm64
+  ./docker manifest push "$image:latest"
 fi
