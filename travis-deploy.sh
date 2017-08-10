3ec7367972f82eb1c8f39882e56eb2a65609e22f
Stefan Scherer <scherer_stefan@icloud.com>
2017-08-10 19:58:39 +0200

    Add -D

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -30,24 +30,26 @@ if [ "$ARCH" == "amd64" ]; then
   mv docker-linux-amd64 docker
   chmod +x docker
   ./docker version
-
+  
+  set -x
+  
   echo "Pushing manifest $image:$TRAVIS_TAG"
-  ./docker manifest create "$image:$TRAVIS_TAG" \
+  ./docker -D manifest create "$image:$TRAVIS_TAG" \
     "$image:linux-amd64-$TRAVIS_TAG" \
     "$image:linux-arm-$TRAVIS_TAG" \
     "$image:linux-arm64-$TRAVIS_TAG" \
     "$image:windows-amd64-$TRAVIS_TAG"
   ./docker manifest annotate "$image:$TRAVIS_TAG" "$image:linux-arm-$TRAVIS_TAG" --os linux --arch arm
   ./docker manifest annotate "$image:$TRAVIS_TAG" "$image:linux-arm64-$TRAVIS_TAG" --os linux --arch arm64
-  ./docker manifest push "$image:$TRAVIS_TAG"
+  ./docker -D manifest push "$image:$TRAVIS_TAG"
 
   echo "Pushing manifest $image:latest"
-  ./docker manifest create "$image:latest" \
+  ./docker -D manifest create "$image:latest" \
     "$image:linux-amd64-$TRAVIS_TAG" \
     "$image:linux-arm-$TRAVIS_TAG" \
     "$image:linux-arm64-$TRAVIS_TAG" \
     "$image:windows-amd64-$TRAVIS_TAG"
   ./docker manifest annotate "$image:latest" "$image:linux-arm-$TRAVIS_TAG" --os linux --arch arm
   ./docker manifest annotate "$image:latest" "$image:linux-arm64-$TRAVIS_TAG" --os linux --arch arm64
-  ./docker manifest push "$image:latest"
+  ./docker manifest -D push "$image:latest"
 fi
