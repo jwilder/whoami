26ad60b0da50ab55664281f8e7bdef482d8c102a
Stefan Scherer <scherer_stefan@icloud.com>
2018-02-01 07:41:58 +0100

    Remove docker update

--- travis-build.sh
+++ travis-build.sh
@@ -2,11 +2,6 @@
 set -e
 
 docker version
-uname -a
-echo "Updating Docker engine to have multi-stage builds"
-sudo service docker stop
-curl -fsSL https://get.docker.com/ | sudo sh
-docker version
 
 if [ -d tmp ]; then
   docker rm build
